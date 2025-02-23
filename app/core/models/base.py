c75319431e724585daae3ef2570d3eb1786a9714
artem <agasyan222@gmail.com>
2025-02-23 21:02:59 +0400

    add DateCreatedUpdatedMixin

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from datetime import datetime, UTC
 
 
 class Base(DeclarativeBase):
@@ -7,3 +8,9 @@ class Base(DeclarativeBase):
 
 class IntegerIDMixin:
     id: Mapped[int] = mapped_column(primary_key=True)
+
+
+class DateCreatedUpdatedMixin:
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now(UTC), nullable=True)
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(UTC), onupdate=datetime.now(UTC), nullable=True)
+
--- app/core/models/notes.py
+++ app/core/models/notes.py
@@ -1,10 +1,10 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from .base import Base, IntegerIDMixin
+from .base import Base, IntegerIDMixin, DateCreatedUpdatedMixin
 
 
-class NoteORM(Base, IntegerIDMixin):
+class NoteORM(Base, IntegerIDMixin, DateCreatedUpdatedMixin):
     __tablename__ = "notes"
 
     title: Mapped[str] = mapped_column(String(length=25))
