c7f17cbf20cd74370cadc8c50fd1d2e8c8c87239
artem <agasyan222@gmail.com>
2025-02-26 21:58:52 +0400

    change 'int -> UserIDType' in UserORM

--- app/core/models/users.py
+++ app/core/models/users.py
@@ -5,9 +5,10 @@ from fastapi_users.db import (
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .base import Base, IntegerIDMixin
+from ..types.user_id import UserIDType
 
 
-class UserORM(Base, IntegerIDMixin, SQLAlchemyBaseUserTable[int]):
+class UserORM(Base, IntegerIDMixin, SQLAlchemyBaseUserTable[UserIDType]):
     @classmethod
     def get_db(cls, session: AsyncSession):
         return SQLAlchemyUserDatabase(session, cls)
