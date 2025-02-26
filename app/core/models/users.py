34ba4c808dd2d14e010411740a8fc8567e517226
artem <agasyan222@gmail.com>
2025-02-26 21:45:01 +0400

    add method for UserORM

--- app/core/models/users.py
+++ app/core/models/users.py
@@ -1,6 +1,13 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users.db import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase
+)
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from .base import Base, IntegerIDMixin
 
 
 class UserORM(Base, IntegerIDMixin, SQLAlchemyBaseUserTable[int]):
-    ...
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, cls)
