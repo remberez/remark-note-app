a97b7009a8541f9e7ecb20501aabbde2b16e5fbf
artem <agasyan222@gmail.com>
2025-02-27 18:15:46 +0400

    move dependencies to inner package

--- app/api/dependencies/auth/fastapi_users.py
+++ app/api/dependencies/auth/fastapi_users.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 
-from api.dependencies.backend import auth_backend
-from api.dependencies.user_manager import get_user_manager
+from api.dependencies.auth.backend import auth_backend
+from api.dependencies.auth.user_manager import get_user_manager
 from core.models import UserORM
 from core.types.user_id import UserIDType
 
