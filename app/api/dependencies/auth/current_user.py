a66147535d4e26e66d2e21811fef722b4f92332b
artem <agasyan222@gmail.com>
2025-02-28 16:45:45 +0400

    fix current_active_verify_user

--- app/api/dependencies/auth/current_user.py
+++ app/api/dependencies/auth/current_user.py
@@ -4,4 +4,4 @@ from .fastapi_users import fastapi_users
 current_user = fastapi_users.current_user()
 current_user_optional = fastapi_users.current_user(optional=True)
 current_active_user = fastapi_users.current_user(active=True)
-current_active_verify_user = fastapi_users.current_user(active=True, optional=True)
+current_active_verify_user = fastapi_users.current_user(active=True, verified=True)
