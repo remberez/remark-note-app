bd44734c042574e46daa0b3a7b3a6f90e6dcca48
artem <agasyan222@gmail.com>
2025-02-28 16:31:06 +0400

    add current_user dependencies

---
+++ app/api/dependencies/auth/current_user.py
@@ -0,0 +1,7 @@
+from .fastapi_users import fastapi_users
+
+
+current_user = fastapi_users.current_user()
+current_user_optional = fastapi_users.current_user(optional=True)
+current_active_user = fastapi_users.current_user(active=True)
+current_active_verify_user = fastapi_users.current_user(active=True, optional=True)
