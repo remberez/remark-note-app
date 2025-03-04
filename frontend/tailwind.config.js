60379258ee695c9f1b05d77a25052d4a9e4dbfdd
artem <agasyan222@gmail.com>
2025-03-04 07:49:32 +0400

    frontend initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      darkBlue: "#151026"
+    },
+  },
+  plugins: [],
+}
+
