1007dbdbe97c072ef8c87c9aba9c05f9cb20a6d2
artem <agasyan222@gmail.com>
2025-03-04 11:00:41 +0400

    add setuser and checkAuth methods

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,17 +1,37 @@
 import { makeAutoObservable } from "mobx";
 import AuthService from "../api/authService";
+import UserService from "../api/userService";
 
 class AuthStore {
     isAuth = false;
+    user = {};
 
     constructor() {
         makeAutoObservable(this);
     }
 
+    setIsAuth(value) {
+        this.isAuth = value;
+    }
+
     async login(username, password) {
         const response = await AuthService.login(username, password);
         localStorage.setItem("token", response.data.access_token);
-        this.isAuth = true;
+        this.setIsAuth(true);
+        this.setUser();
+    }
+
+    async setUser() {
+        if (this.isAuth) {
+            this.user = await UserService.fetchMe();
+        }
+    }
+
+    async checkAuth() {
+        if (localStorage.getItem("token")) {
+            this.setIsAuth(true);
+            this.setUser();
+        }
     }
 }
 
