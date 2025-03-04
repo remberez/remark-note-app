00f3b60f9d27566b983972424b7ee8eca9561aa8
artem <agasyan222@gmail.com>
2025-03-04 13:35:06 +0400

    authform design rework

--- frontend/src/components/auth/AuthForm.jsx
+++ frontend/src/components/auth/AuthForm.jsx
@@ -1,5 +1,7 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../../App";
+import Button from '@mui/material/Button';
+import { TextField } from "@mui/material";
 
 const AuthForm = () => {
     const authStore = useContext(AuthContext);
@@ -13,26 +15,33 @@ const AuthForm = () => {
     const [password, setPassword] = useState("");
 
     return (
-        <section className="w-max container mt-20 bg-white px-5 py-3">
+        <section className="w-max container mt-20 bg-white px-12 py-7 rounded">
             <h2 className="text-center text-xl mb-4 font-bold">
                 Авторизация
             </h2>
             <form onSubmit={onSubmit} method="post">
-                <div className="mb-4 flex gap-x-2">
-                    <label htmlFor="username">
-                        Логин
-                    </label>
-                    <input type="text" name="username" id="username" value={username} onChange={e => setUsername(e.target.value)}/>
+                <div className="flex flex-col gap-y-4">
+                    <TextField 
+                        id="username" 
+                        label="Логин" 
+                        variant="outlined" 
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        size="small"
+                    />
+                    <TextField 
+                        id="password" 
+                        label="Пароль" 
+                        variant="outlined" 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}
+                        size="small"
+                        type="password"
+                    />
+                    <Button variant="contained" type="submit">
+                        Войти
+                    </Button>
                 </div>
-                <div className="mb-4 flex gap-x-2">
-                    <label htmlFor="password">
-                        Пароль
-                    </label>
-                    <input type="password" name="password" id="password" value={password} onChange={e => setPassword(e.target.value)}/>
-                </div>
-                <button type="submit" className="bg-lightDarkBlue text-white px-5 py-2 block w-full">
-                    Войти
-                </button>
             </form>
         </section>
     )
