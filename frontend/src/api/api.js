a6701b2bae4d0d52b0371f847239a09a074c4287
artem <agasyan222@gmail.com>
2025-03-04 08:16:31 +0400

    add axios config

---
+++ frontend/src/api/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const url = "http://localhost:8000/api";
+
+const $api = axios.create({
+    baseURL: url,
+    headers: {
+        "Content-Type": "application/json",
+    }
+})
+
+$api.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+)
