2fb7dd5484ab89bb5c848cda2575eb96dd97be8f
artem <agasyan222@gmail.com>
2025-03-11 16:33:20 +0400

    add get_user_notes method

--- app/core/services/notes_service.py
+++ app/core/services/notes_service.py
@@ -1,8 +1,15 @@
+from sqlalchemy import Sequence
+
+from core.models.notes import NoteORM
 from core.repository.abc_repository import AbstractRepository
 from core.services.base import BaseService
 
 
 class NoteService(BaseService):
+    Model = NoteORM
+
     def __init__(self, repository: AbstractRepository):
         super().__init__(repository)
 
+    async def get_user_notes(self, user_id: int) -> Sequence[Model]:
+        return await self.repository.list(user_id=user_id)
