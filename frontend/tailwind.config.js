9a4a7f6b76fd7ae175cffbce35c96be7d34d2115
artem <agasyan222@gmail.com>
2025-03-14 16:27:18 +0400

    add helpers

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,22 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          xl: "1200px",
+          "2xl": "1200px",
+        }
+      },
+      colors: {
+        black: "#111114",
+        grayText: "#B3B3B3",
+      },
+      fontFamily: {
+        unbounded: ["Unbounded", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
