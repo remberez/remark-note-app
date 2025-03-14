58c8f31c0c452b6d35d0a919f414a9a125dae8d5
artem <agasyan222@gmail.com>
2025-03-14 08:12:43 +0400

    remove extra async manager

--- app/core/repository/notes.py
+++ app/core/repository/notes.py
@@ -44,50 +44,44 @@ class NoteRepository(AbstractRepository[NoteModel], ABC):
 
 class SQLAlchemyNoteRepository(SQLAlchemyAbstractRepository[NoteORM], NoteRepository):
     async def create(self, **data) -> NoteORM:
-        async with self._session as session:
-            note = NoteORM(**data)
-            session.add(note)
-            await session.commit()
-            await session.refresh(note)
-            return note
+        note = NoteORM(**data)
+        self._session.add(note)
+        await self._session.commit()
+        await self._session.refresh(note)
+        return note
 
     async def delete(self, note_id: int) -> None:
-        async with self._session as session:
-            await session.execute(delete(NoteORM).where(NoteORM.id == note_id))
-            await session.commit()
+        await self._session.execute(delete(NoteORM).where(NoteORM.id == note_id))
+        await self._session.commit()
 
     async def update(self, note_id: int, **data) -> NoteORM:
-        async with self._session as session:
-            result = await session.execute(
-                update(NoteORM).where(NoteORM.id == note_id).values(**data).returning(NoteORM)
-            )
-            await session.commit()
-            return result.scalar_one()
+        result = await self._session.execute(
+            update(NoteORM).where(NoteORM.id == note_id).values(**data).returning(NoteORM)
+        )
+        await self._session.commit()
+        return result.scalar_one()
 
     async def list(self, user_id: int, order_by: str, in_favorites: bool, is_desc: bool) -> Sequence[NoteORM]:
-        async with self._session as session:
-            stmt = select(NoteORM).where(NoteORM.user_id == user_id)
+        stmt = select(NoteORM).where(NoteORM.user_id == user_id)
 
-            if in_favorites is not None:
-                stmt = stmt.where(NoteORM.in_favorites == in_favorites)
+        if in_favorites is not None:
+            stmt = stmt.where(NoteORM.in_favorites == in_favorites)
 
-            if order_by:
-                if is_desc:
-                    stmt = stmt.order_by(desc(getattr(NoteORM, order_by)))
-                else:
-                    stmt = stmt.order_by(asc(getattr(NoteORM, order_by)))
+        if order_by:
+            if is_desc:
+                stmt = stmt.order_by(desc(getattr(NoteORM, order_by)))
+            else:
+                stmt = stmt.order_by(asc(getattr(NoteORM, order_by)))
 
-            result = await session.execute(stmt)
-            return result.scalars().all()
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
 
     async def get(self, note_id: int) -> NoteORM:
-        async with self._session as session:
-            stmt = select(NoteORM).filter_by(id=note_id)
-            result = await session.execute(stmt)
-            return result.scalar_one_or_none()
+        stmt = select(NoteORM).filter_by(id=note_id)
+        result = await self._session.execute(stmt)
+        return result.scalar_one_or_none()
 
     async def find_by_title(self, title: str) -> Sequence[NoteORM]:
-        async with self._session as session:
-            stmt = select(NoteORM).where(NoteORM.title.like("%" + title + "%"))
-            result = await session.execute(stmt)
-            return result.scalars().all()
+        stmt = select(NoteORM).where(NoteORM.title.like("%" + title + "%"))
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
