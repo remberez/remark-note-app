d9ad84517634dab46f7d70e7292a6aa8a084b5e7
artem <agasyan222@gmail.com>
2025-03-17 15:56:57 +0400

    add margin bottom

--- frontend/src/components/MainHeader.jsx
+++ frontend/src/components/MainHeader.jsx
@@ -3,10 +3,10 @@ const MainHeader = () => {
         <div className="bg-pageColor">
             <section className="bg-black pt-[140px] pb-[240px] rounded-b-[15%] shadow-xl">
                 <div className="container text-white">
-                    <h2 className="text-4xl">
+                    <h2 className="text-4xl mb-4">
                         Отточите своё мышление.
                     </h2>
-                    <p className="text-xl text-grayText mb-[75px]">
+                    <p className="text-xl text-grayText mb-[75px] max-w-[500px]">
                         Бесплатное и гибкое приложение для ваших личных мыслей.
                     </p>
                     <button className="bg-green px-[70px] py-[10px] rounded-lg">
